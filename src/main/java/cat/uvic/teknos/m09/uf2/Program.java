90e076cfbcb2a9e615ef083885c40a53a64fef5b
Aldo Matias Lazo Rocha <aldomatias.lazo@uvic.cat>
2022-10-28 20:05:39 +0200

    Implemented a thread generated from a method that cheks every 60 seconds if the hash.properties file  has been changed and if it has, it updates the hashing configuration

--- src/main/java/cat/uvic/teknos/m09/uf2/Program.java
+++ src/main/java/cat/uvic/teknos/m09/uf2/Program.java
@@ -1,6 +1,7 @@
 package cat.uvic.teknos.m09.uf2;
 
 import java.io.IOException;
+import java.lang.reflect.Parameter;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,29 +10,57 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Properties;
 import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Program {
     private static boolean follow = true;
     private static Scanner in = new Scanner(System.in);
+    private static HashParameters hashParameters;
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, URISyntaxException, IOException {
-        var properties = new Properties();
+    private static Properties properties;
+
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, URISyntaxException, IOException, InterruptedException {
+        properties = new Properties();
         properties.load(Program.class.getResourceAsStream("/hash.properties"));
 
-        var hashParameters = new HashParameters(properties.getProperty("algorithm"), properties.getProperty("salt"));
+        hashParameters = new HashParameters(properties.getProperty("algorithm"), properties.getProperty("salt"));
+
+        var t1 = new Thread(Program::updateHash,"t1 comprovant");
+        t1.start();
 
         while (follow) {
             System.out.println("Type the path of the file you want to hash");
             System.out.println("Digest: " + getDigest(in.nextLine(), hashParameters));
             System.out.println("Salt: " + hashParameters.getSalt());
-
             askToFollow();
         }
+
         System.out.println("Bye!");
 
+    }
+    public static void updateHash() {
+        while (follow){
+            try {
+                properties.load(Program.class.getResourceAsStream("/hash.properties"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                Thread.sleep(1000*60);
+                if (!properties.getProperty("salt").equals(hashParameters.getSalt())||!properties.getProperty("algorithm").equals(hashParameters.getAlgorithm())) {
+                    hashParameters.setSalt(properties.getProperty("salt"));
+                    hashParameters.setAlgorithm(properties.getProperty("algorithm"));
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
-
     private static void askToFollow() {
         System.out.println("Type 'e' to exit or just enter to follow");
         var exit = in.nextLine();
